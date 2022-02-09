f797b0e854af5be621ca030d2e67a1f77b0a4073
Hamza ZERHOUNI <hamza.zerhouni@ama.bzh>
2022-02-09 12:43:22 +0100

    first commit

---
+++ vscode_cpu_load/__init__.py
@@ -0,0 +1,5 @@
+"""Test package."""
+
+__version__ = "1.0.0"
+__pretty_name__ = __name__.replace("_", "-")
+__description__ = "Test"
---
+++ vscode_cpu_load/__main__.py
@@ -0,0 +1,9 @@
+"""Test."""
+
+import sys
+
+
+if __name__ == "__main__":
+    """Program main."""
+
+    sys.stdout.write("Ok\n")
